cea31cee3b084e7c73a8c121aaf921c894f88543
vukbyk <vukbyk@gmail.com>
2025-04-20 07:36:08 +0200

    LAST commit before big cleenup No my comments after this Pre velikog ciscenja posle ovoga nestaju moji komentari

--- src/glwindow.cpp
+++ src/glwindow.cpp
@@ -733,27 +733,3 @@ Frustum GLWindow::createFrustumFromCamera(const Transform& cam, float nearOffset
 
     return frustum;
 }
-
-//void GLWindow::initQuadForFB()
-//{
-//    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
-//    // positions   // texCoords
-//    -1.0f,  1.0f,   0.0f, 1.0f,
-//    -1.0f, -1.0f,   0.0f, 0.0f,
-//     1.0f, -1.0f,   1.0f, 0.0f,
-
-    //    -1.0f,  1.0f,   0.0f, 1.0f,
-    //     1.0f, -1.0f,   1.0f, 0.0f,
-    //     1.0f,  1.0f,   1.0f, 1.0f
-    //    };
-
-    //    glGenVertexArrays(1, &quadVAO);
-    //    glGenBuffers(1, &quadVBO);
-    //    glBindVertexArray(quadVAO);
-    //    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    //    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-    //    glEnableVertexAttribArray(0);
-    //    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    //    glEnableVertexAttribArray(1);
-    //    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-    //}
